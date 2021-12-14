e5c022e3351701912c9cba887499b1004d7d7b16
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2021-12-13 21:05:39 -0500

    refactor with some elixir-isms

--- aoc-12-1.exs
+++ aoc-12-1.exs
@@ -1,50 +1,43 @@
 defmodule AOC12 do
-  def find_paths(rules, current, visited) do
-    if current == "end" do
-      1
-    else
-      new_visited =
-        if !(String.upcase(current) == current) do
-          MapSet.put(visited, current)
-        else
-          visited
-        end
+  def find_paths(_rules, "end", _visited), do: 1
+  def find_paths(rules, current, old_visited) do
+    new_visited =
+      if !(String.upcase(current) == current) do
+        MapSet.put(old_visited, current)
+      else
+        old_visited
+      end
 
-      Enum.reduce(rules[current] || [], 0, fn node, total ->
-        if MapSet.member?(new_visited, node) do
-          total
-        else
-          total + find_paths(rules, node, new_visited)
-        end
-      end)
-    end
+    rules
+    |> Map.get(current)
+    |> Enum.reduce(0, fn node, total ->
+      if MapSet.member?(old_visited, node) do
+        total
+      else
+        total + find_paths(rules, node, new_visited)
+      end
+    end)
   end
 end
 
-stream = File.stream!("aoc-12.txt")
-
 input =
-  Enum.map(
-    Enum.into(stream, []),
-    fn x -> String.replace(x, "\n", "") end
-  )
+  File.stream!("aoc-12.txt")
+  |> Stream.map(&String.trim/1)
+  |> Enum.map(fn x-> x end)
+  |> Enum.into([])
 
 rules =
-  Enum.reduce(input, %{}, fn line, map ->
-    split = String.split(line, "-")
+  input
+  |> Enum.map(fn line -> String.split(line, "-") end)
+  |> Enum.reduce(%{}, fn line, map ->
+    [head, tail] = line
 
-    remapped =
-      if Map.has_key?(map, hd(split)) do
-        Map.put(map, hd(split), tl(split) ++ map[hd(split)])
-      else
-        Map.put(map, hd(split), tl(split))
-      end
-
-    if Map.has_key?(remapped, hd(tl(split))) do
-      Map.put(remapped, hd(tl(split)), [hd(split) | remapped[hd(tl(split))]])
-    else
-      Map.put(remapped, hd(tl(split)), [hd(split)])
-    end
+    map
+    |> Map.update(head, [tail], fn existing_head -> [tail | existing_head] end)
+    |> Map.update(tail, [head], fn existing_tail -> [head | existing_tail] end)
+    # |> IO.inspect()
   end)
 
-IO.puts(AOC12.find_paths(rules, "start", MapSet.new()))
+result = AOC12.find_paths(rules, "start", MapSet.new())
+
+IO.puts(result)
